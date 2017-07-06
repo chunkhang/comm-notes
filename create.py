d7318ca2afe6ddde9fedf6b7abbd6e60ace07f35
Marcus Mu <chunkhang@gmail.com>
2017-07-06 16:50:19 +0800

    Fix formatting

--- create.py
+++ create.py
@@ -9,18 +9,24 @@ def main():
 	with open('README.md', 'w') as file:
 		file.write('# Communication Skills Notes\n')
 		file.write('\n')
-		file.write('Tabulated notes for Communication Skills final examination\n')
-		file.write('\n')
-		file.write('Data by: Kai Wern \n')
+		file.write('Tabulated notes for Communication Skills final examination ' +
+			'<br/>\n')
+		file.write('<br/>\n')
+		file.write('Data by: Kai Wern <br/>\n')
 		file.write('Edited by: Marcus Mu\n')
 		file.write('\n')
-		file.write('## Table of Contents\n')
+		file.write('#### Table of Contents\n')
 		file.write('\n')
-		file.write('[TOC]')
+		file.write('* [Chapter 1](#chapter-1)\n')
+		file.write('* [Chapter 3](#chapter-3)\n')
+		file.write('* [Chapter 5](#chapter-5)\n')
+		file.write('* [Chapter 6](#chapter-6)\n')
 		n = 0
 		firstExample = True
 		for line in lines:
 			if '##' in line:
+				file.write('\n\n')
+				file.write('<a name="%s"/>' % ('-'.join(line.lower().split()[1:])))
 				file.write('\n\n')
 				file.write(line+'\n')
 				file.write('\n')
@@ -31,16 +37,16 @@ def main():
 				n += 1
 				firstExample = True
 				file.write('\n')
-				file.write('%s | **%s** | ' % ('0'+str(n) if len(str(n))==1 else str(n),
-					line[2:]))
+				file.write('%s | <p align="center">**%s**</p> | ' % 
+					('0'+str(n) if len(str(n))==1 else str(n), line[2:]))
 			elif '@' in line:
 				if len(line) <= 2:
-					file.write('<p style="text-align: center;">-</p> | ')
+					file.write('<p align="center">-</p> | ')
 				else:
 					file.write('%s | ' % line[2:])
 			elif '%' in line:
 				if len(line) <= 2:
-					file.write('<p style="text-align: center;">-</p>')
+					file.write('<p align="center">-</p>')
 				else:
 					if firstExample:
 						file.write('<li>%s</li> ' % line[2:])
